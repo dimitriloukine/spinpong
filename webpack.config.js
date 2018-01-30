d7862300cacea0d057bc34ffdd1cd3c5a9dba682
Dimitri Loukine <dlo@wine-logics.com>
2018-01-30 23:55:34 +0100

    webpack config with scss & typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,42 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+	entry: './src/index.ts',
+	devtool: 'source-map',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	devServer: {
+    contentBase: './dist'
+  },
+	module: {
+		rules: [
+		{
+      test: /\.ts$/,
+      use: 'ts-loader',
+      exclude: /node_modules/
+    },
+		{
+			test: /\.scss$/,
+				use: [
+					{
+						loader: "style-loader" // creates style nodes from JS strings
+					}, 
+					{
+						loader: "css-loader", 
+						options: {
+							sourceMap: true
+						}
+					}, 
+					{
+					loader: "sass-loader", 
+						options: {
+							sourceMap: true
+						}
+					}
+				]
+			}
+		]
+	}
 };
